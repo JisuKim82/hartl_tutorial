90fd0ca946e8cd7ce0cd86e719d8c12ee762d551
Jisu Kim <jisu.kim@flatironschool.com>
2014-05-20 14:25:52 -0400

    Update Gemfile and production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-HartlTutorial::Application.application.configure do
+Ruby.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
